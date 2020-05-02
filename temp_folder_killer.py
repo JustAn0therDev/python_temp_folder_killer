e3bb53d69a39fd35725a24ea45f4eb3677476352
Ruan Montelo <ruanmontelo@gmail.com>
2020-05-02 19:42:37 -0300

    Fixing some of the logic and using the glob recursion to find everything inside the temp folder to delete it

--- temp_folder_killer.py
+++ temp_folder_killer.py
@@ -3,39 +3,28 @@ import glob
 from temp_folder_path import PATH_TO_TEMP_FOLDER
 
 def main():
-    path_to_temp_folder_without_asterisk = PATH_TO_TEMP_FOLDER[:-1]
-    total_of_deleted_directories = 0
-    total_of_deleted_files = 0
-    list_of_files_in_directory = glob.glob(PATH_TO_TEMP_FOLDER)
-    list_of_subdirectories_in_directory = os.listdir(path_to_temp_folder_without_asterisk)
-    total_files = len(list_of_files_in_directory)
-    total_directories = len(list_of_subdirectories_in_directory)
+    total_deleted = 0
+    list_of_files_and_directories = glob.glob(PATH_TO_TEMP_FOLDER, recursive=True)
+    total = len(list_of_files_and_directories)
 
     user_response = input('Do you want to see all temporary files/directories? (y - Y/n - N): ')
     if (user_response.lower() == 'y'):
         print('Ok, this is the list of files currently: \n')
-        for file in list_of_files_in_directory:
+        for file in list_of_files_and_directories:
             print(file)
 
     user_response = input('Do you want to delete all of these files? (y - Y/n - N): ')
 
     if (user_response.lower() == 'y'):
         print('Deleting files...')
-        for file in list_of_files_in_directory:
+        for file in list_of_files_and_directories:
             try:
                 os.remove(file)
-                total_of_deleted_files += 1
+                total_deleted += 1
             except Exception as exception_message:
                 print("Can't delete file " + file + " because of the following error: " + str(exception_message) + ". Going for the next one.")
 
-        for subdirectory in list_of_subdirectories_in_directory:
-            try:
-                os.remove(subdirectory)
-                total_of_deleted_directories += 1
-            except Exception as exception_message:
-                print("Can't delete directory " + subdirectory + " because of the following error: " + str(exception_message) + ". Going for the next one.")
-        
-        print(f'Finished. Total of files: {str(total_files)} | Total of deleted files: {str(total_of_deleted_files)}\nTotal Directories: {total_directories} | Total of deleted directories: {total_of_deleted_directories}')
+        print(f'Finished. Total of files/directories: {str(total)} | Total of deleted files/directories: {str(total_deleted)}')
     else:
         print("Ok, I won't do anything to it.")
 
