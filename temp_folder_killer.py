e37fdf95c37fb62b9b89df8bb36efccf491e1104
Ruan Montelo <ruanmontelo@gmail.com>
2020-06-15 16:06:43 -0300

    Changing the name of the main function and setting the code to be a bit smaller and more expressive to what its doing

--- temp_folder_killer.py
+++ temp_folder_killer.py
@@ -4,28 +4,30 @@ from colorama import Fore, Style
 from temp_folder_path import PATH_TO_TEMP_FOLDER
 from terminal_style_functions import print_message_with_color
 
-def main():
-    total_deleted = 0
+def temp_folder_killer():
+    total_files_deleted = 0
     list_of_files_and_directories = glob.glob(PATH_TO_TEMP_FOLDER, recursive=True)
     total = len(list_of_files_and_directories)
 
-    user_response = input('Do you want to delete all files in the current temp folder? (y - Y/n - N or any other character): ').lower()
+    user_response = input('Do you want to delete all files in the temp folder? (y - Y/n - N or any other character): ').lower()
 
-    if (user_response == 'y'):
-        print('Deleting files...\n')
+    if (user_response.lower() == 'y'):
+        print('Attemping to delete files...\n')
 
         for file in list_of_files_and_directories:
             try:
                 os.remove(file)
                 print_message_with_color(f'File {str(file)} deleted!', Fore.GREEN)
-                total_deleted += 1
+                total_files_deleted += 1
             except Exception as exception_message:
                 print_message_with_color(
-                    f"Can't delete file {str(file)} because of the following error: {str(exception_message)}. Going for the next one.",
+                    f"Can't delete file {str(file)} because of the following error: {str(exception_message)}.",
                     Fore.RED)
 
-        print_message_with_color(f'Finished. Total of files/directories: {str(total)} | Total of deleted files/directories: {str(total_deleted)}',
-        Fore.GREEN)
+        print_message_with_color(
+            f'Finished. Total of files/directories: {str(total)} | Total of deleted files/directories: {str(total_deleted)}',
+            Fore.GREEN)
     else:
         print("Ok, I won't do anything to it.")
-main()
+
+temp_folder_killer()
